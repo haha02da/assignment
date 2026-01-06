7287cc420f1b9f53951c80152a9256e7885cfd66
Dongmin Kim <dongmin2685@gmail.com>
2026-01-06 17:53:40 +0900

    homework1

---
+++ dongmin/work1.py
@@ -0,0 +1,16 @@
+def is_balanced_parentheses(s: str) -> bool:
+    stack = []
+    for i in s:
+        if i=='(':
+            stack.append(i)
+        elif i==')':
+            if not stack:
+                return False
+            stack.pop()
+    return len(stack) == 0
+
+print(is_balanced_parentheses("(()())"))  # True
+print(is_balanced_parentheses("(()"))     # False
+print(is_balanced_parentheses("())"))     # False
+print(is_balanced_parentheses("(hello(world))"))     # True
+print(is_balanced_parentheses(")(abc("))     # False
