30226884e3dc6e955c6a252840ac80c5076ccaec
bi <skkuaws0215@gmail.com>
2026-01-06 17:19:27 +0900

    과제3

---
+++ skkuaws0215/과제1.py
@@ -0,0 +1,10 @@
+def is_balanced_parentheses(s: str) -> bool:
+    # 여기에 코드를 작성하면 됩니다.
+
+
+print(is_balanced_parentheses("(()())"))  # True
+print(is_balanced_parentheses("(()"))     # False
+print(is_balanced_parentheses("())"))     # False
+print(is_balanced_parentheses("(hello(world))"))     # True
+print(is_balanced_parentheses(")(abc("))     # False
+print()
