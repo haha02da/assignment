bfd3fa7f39e38d6567162cfed1101e884ccb4fe0
GiTae <bgt1674@gmail.com>
2026-01-07 16:02:24 +0900

    real final

--- milk-case/assignment_7.py
+++ milk-case/assignment_7.py
@@ -2,6 +2,10 @@ import random
 import time
 import sys
 
+class OutOfRangeError(Exception):
+    def __init__(self):
+        super().__init__('Enter only numbers between 1 to 20!')
+
 def up_down_game():
     answer = random.randint(1,20)
     print("Random Number Generated!")
@@ -12,7 +16,11 @@ def up_down_game():
         while True:
             try:
                 user_input = int(sys.stdin.readline())
+                if user_input < 1 or user_input > 20:
+                    raise OutOfRangeError
                 break
+            except OutOfRangeError as e:
+                print(e)
             except ValueError:
                 print('Enter only numbers between 1 to 20!')
 
